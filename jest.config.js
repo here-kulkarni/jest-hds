8045c1f0e368c14788267c6ad7d852fd0cd63082
pradnya <pradnya.kulkarni@here.com>
2021-09-22 20:16:09 +0530

    repo corrected

--- jest.config.js
+++ jest.config.js
@@ -22,8 +22,8 @@ module.exports = {
     },
   },
   testMatch: [
-    "<rootDir>/src/**/__tests__/**/*.{js,jsx,ts,tsx}",
-    "<rootDir>/src/**/*.{spec,test}.{js,jsx,ts,tsx}",
+    "<rootDir>/src/*.{js,jsx,ts,tsx}",
+    "<rootDir>/src/*.{spec,test}.{js,jsx,ts,tsx}",
   ],
   transform: {
     "^.+\\.(js|jsx|ts)$": "babel-jest",
